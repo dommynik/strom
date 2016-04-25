ccd3df5ac75416be4f0e0ea8f4d739b8b528d53f
Dominik Zosgornik <mail@dommynik.de>
2016-04-25 09:49:43 +0200

    Added new macro seperated1

--- src/macros/seperated.rs
+++ src/macros/seperated.rs
@@ -31,3 +31,29 @@ macro_rules! seperated0 {
         seperated0!($src, call!($pat), call!($sep))
     );
 }
+
+#[macro_export]
+macro_rules! seperated1 {
+    ($src:expr, $pat:ident!($($args:tt)*), $sep:ident!($($args2:tt)*)) => (
+        match seperated0!($src, $pat!($($args)*), $sep!($($args2)*)) {
+            $crate::Output {src, mat: Ok(mat)} => if mat.val.len() > 0 {
+                $crate::Output::ok(src, mat)
+            } else {
+                $crate::Output::err($src, format!("No match."))
+            },
+            _ => $crate::Output::err($src, format!("Impossible."))
+        }
+    );
+
+    ($src:expr, $pat:ident!($($args:tt)*), $sep:path) => (
+        seperated1!($src, $pat!($($args)*), call!($sep))
+    );
+
+    ($src:expr, $pat:path, $sep:ident!($($args2:tt)*)) => (
+        seperated1!($src, call!($pat), $sep!($($args2)*))
+    );
+
+    ($src:expr, $pat:path, $sep:path) => (
+        seperated1!($src, call!($pat), call!($sep))
+    );
+}
